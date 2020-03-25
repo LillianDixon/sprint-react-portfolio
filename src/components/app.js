415e105874e0d0e26a9e0c0e0b429e46149e8db0
Lillian Dixon <dixon.lillian92@gmail.com>
2020-03-25 15:18:30 -0600

    finished portfolio-manager, started blog

--- src/components/app.js
+++ src/components/app.js
@@ -1,16 +1,25 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import axios from "axios";
-
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faTrash,
+  faSignOutAlt,
+  faEdit
+} from "@fortawesome/free-solid-svg-icons";
 import NavigationContainer from "./navigation/navigation-container";
 import Home from "./pages/home";
 import About from "./pages/about";
 import Contact from "./pages/contact";
 import Blog from "./pages/blog";
+import BlogDetail from "./pages/blog-detail";
 import PortfolioManager from "./pages/portfolio-manager";
 import PortfolioDetail from "./portfolio/portfolio-detail";
 import Auth from "./pages/auth";
 import NoMatch from "./pages/no-match";
+
+library.add(faTrash, faSignOutAlt, faEdit);
 
 export default class App extends Component {
   constructor(props) {
@@ -92,7 +101,7 @@ export default class App extends Component {
               loggedInStatus={this.state.loggedInStatus}
               handleSuccessfulLogout={this.handleSuccessfulLogout}
             />
-            <h1>{this.state.loggedInStatus}</h1>
+            {/* <h1>{this.state.loggedInStatus}</h1> */}
             <Switch>
               <Route exact path="/" component={Home} />
 
@@ -110,6 +119,7 @@ export default class App extends Component {
               <Route path="/about-me" component={About} />
               <Route path="/contact" component={Contact} />
               <Route path="/blog" component={Blog} />
+              <Route path="/b/:slug" component={BlogDetail} />
               {this.state.loggedInStatus === "LOGGED_IN" ? (
                 this.authorizedPages()
               ) : null}
