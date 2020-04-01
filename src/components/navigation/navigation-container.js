d96af858a2b441f6b12e86ae60e819ff7485d92d
Lillian Dixon <dixon.lillian92@gmail.com>
2020-04-01 15:38:35 -0600

    finished project

--- src/components/navigation/navigation-container.js
+++ src/components/navigation/navigation-container.js
@@ -63,7 +63,7 @@ const NavigationComponent = props => {
       </div>
 
       <div className="right-side">
-        JORDAN HUDGENS
+        LILLIAN DIXON
         {props.loggedInStatus === "LOGGED_IN" ? (
           <a onClick={handleSignOut}>
             <FontAwesomeIcon icon="sign-out-alt" />
